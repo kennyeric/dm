da32df045c44e41e168a5e608da71bbbfd18ab96
Xu Chuan <kenny.xu.chuan@gmail.com>
2012-01-07 00:47:20 -0500

    add item test data and item test ddl

---
+++ sphinxmysql/ddl/item_test.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `item` (
+      `item_id` int(11) NOT NULL AUTO_INCREMENT,
+      `tags` varchar(2048) NOT NULL,
+      PRIMARY KEY (`item_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
---
+++ sphinxmysql/mysql/item_test.sql
@@ -0,0 +1,4 @@
+insert into item value(null,'配饰,戒指');
+insert into item value(null,'配饰,围巾,红色');
+insert into item value(null,'配饰,围巾,红色,日韩范');
+insert into item value(null,'上衣,羽绒服,白色,日韩范');
