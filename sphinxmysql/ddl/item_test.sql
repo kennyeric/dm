3541f6f95e53b3966871bbfc32238c6b9c4abae6
Xu Chuan <kenny.xu.chuan@gmail.com>
2012-01-07 02:33:53 -0500

    add publish date column

--- sphinxmysql/ddl/item_test.sql
+++ sphinxmysql/ddl/item_test.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `item` (
-      `item_id` int(11) NOT NULL AUTO_INCREMENT,
+      `item_id` int(32) NOT NULL AUTO_INCREMENT,
       `tags` varchar(2048) NOT NULL,
+      `publish_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
       PRIMARY KEY (`item_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
